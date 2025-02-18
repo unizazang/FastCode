1059c33a59d251bac7fe711b26e2f9bd65cc6707
unizazang <zifnffk32123@gmail.com>
2025-02-19 01:30:49 +0900

    php 복구해야됨

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,8 @@
 <?php
     $hostname = 'localhost';
-    $dbuserid = 'fastcode';
-    $dbpasswd = 'yumi1212*';
-    $dbname = 'fastcode';
+    $dbuserid = 'unizazang123';
+    $dbpasswd = 'ehflxhtm1!';
+    $dbname = 'unizazang123';
 
     $mysqli = new mysqli($hostname,$dbuserid, $dbpasswd,$dbname);
     if($mysqli -> connect_errno){
